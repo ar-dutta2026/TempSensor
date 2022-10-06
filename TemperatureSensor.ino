e50fdf9d163946e81b8b55d4a6839defd45b0cd0
Aritra Dutta <112745073+ar-dutta2026@users.noreply.github.com>
2022-10-05 23:58:36 -0700

    Add files via upload

---
+++ TemperatureSensor.ino
@@ -0,0 +1,41 @@
+#include <LiquidCrystal_I2C.h>
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);  
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  dht.begin();     
+  lcd.init();      
+  lcd.backlight(); 
+}
+
+void loop()
+{
+  delay(2000); 
+
+  float humi  = dht.readHumidity();    
+  float tempC = dht.readTemperature(); 
+
+  lcd.clear();
+  // check if any reads failed
+  if (isnan(humi) || isnan(tempC)) {
+    lcd.setCursor(0, 0);
+    lcd.print("Failed");
+  } else {
+    lcd.setCursor(0, 0);  
+    lcd.print("Temp: ");
+    lcd.print(tempC);    
+    lcd.print((char)223); 
+    lcd.print("C");
+
+    lcd.setCursor(0, 1);  
+    lcd.print("Humi: ");
+    lcd.print(humi);      
+    lcd.print("%");
+  }
+}
+
